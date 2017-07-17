e70bb488a6dd397b190e16331abd1e94c53e5b7f
BIWhitfield <beniwhitfield@gmail.com>
2017-07-17 16:50:57 +0100

    [index.ios] update to render App component

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App'
 
-AppRegistry.registerComponent('frontEnd', () => frontEnd);
+AppRegistry.registerComponent('frontEnd', () => App);
