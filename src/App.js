a357cd391b89620f29ea827763387f9d851ef0dc
Leo Duffy <leoduffy@hotmail.com>
2017-07-18 10:48:15 +0100

    [App] created boilerplate in render to render ProfilePage

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import ProfilePage from './components/ProfilePage';
+
+
+class App extends Component {
+    render() {
+        return (
+<ProfilePage />
+          
+        );
+    };
+}
+
+export default App;
