5b552c2dcd3743c8bf40bbfd8a6b885aabc6d2e9
Leo Duffy <leoduffy@hotmail.com>
2017-07-18 17:38:19 +0100

    [common] basic styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import {
     Text,
     View
 } from 'react-native';
-
+import CreateGroups from './components/CreateGroups';
 import ProfilePage from './components/ProfilePage';
-
+import AddEvent from './components/AddEvent';
+import Notifications from './components/Notifications';
+import GroupPage from './components/GroupPage';
+import MyGroups from './components/MyGroups';
 
 class App extends Component {
     render() {
         return (
-<ProfilePage />
+<MyGroups />
           
         );
     };
