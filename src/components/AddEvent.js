0da819ae115367695bce2e1ba76acd6cf416f280
Leo Duffy <leoduffy@hotmail.com>
2017-07-18 12:15:09 +0100

    [AddEvent] static page containing multiple input fields

---
+++ src/components/AddEvent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+
+import Card from './common/Card';
+import CardSection from './common/CardSection';
+import Header from './common/Header';
+import Input from './common/Input';
+const AddEvent = () => {
+    const { imageStyle } = styles;
+    return (
+        <Card>
+            <Header headerText={'Add Event'} />
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: 'https://s1.postimg.org/nkt54pa4f/Wacky-_Races-2016-title.png' }} />
+            </CardSection>
+            <CardSection>
+                <Input placeholder='Date' />
+            </CardSection>
+            <CardSection>
+                <Input placeholder='Organiser' />
+            </CardSection>
+            <CardSection>
+                <Input placeholder='Time' />
+            </CardSection>
+            <CardSection>
+                <Input placeholder='Skills Required' />
+            </CardSection>
+            <CardSection>
+                <Input placeholder='Description' />
+            </CardSection>
+            <CardSection>
+                <Text>EVENTS GROUPS ALERTS SEARCH SETTINGS</Text>
+            </CardSection>
+        </Card>
+    )
+}
+const styles = {
+    imageStyle: {
+        height: 150,
+        flex: 1,
+        width: null
+
+    }
+}
+
+export default AddEvent;
