facf29c0d7093ec06615fa5825a3f3208e81250f
BIWhitfield <beniwhitfield@gmail.com>
2017-07-20 16:00:23 +0100

    [SignUp] revert component display

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,58 +11,16 @@ import CardSection from './common/CardSection';
 import Header from './common/Header';
 import Input from './common/Input';
 
-export default class SignUp extends Component {
-    render() {
-      const { thumbnailStyle,
-        headerContentStyle,
-        thumbnailContainerStyle,
-        headerTextStyle,
-        imageStyle
-        } = styles;
+const SignUp = () => {
+
         return (
-            <Card>
-                <Header headerText='Sign Up' />
-                <CardSection>
-                <Input placeholder='Phone Number'/>
-                </CardSection>
-                <CardSection>
-                    <Text>Area</Text>
-                    </CardSection>
-                     <CardSection>
-                   <Text>Upload Photo</Text>
-                    </CardSection>
-                    <CardSection>
-                        <Image style={imageStyle}source={{uri: 'https://i.ytimg.com/vi/RXKJolS9Atg/hqdefault.jpg'}}/>
-                    </CardSection>
-                </Card>
+<View>
+    <Text>
+        SIGN UP 1
+    </Text>
+</View>
         );
     };
-};
 
-const styles = {
-    headerContentStyle: {
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 18
-    },
-    thumbnailStyle: {
-        height: 50,
-        width: 50
 
-    },
-    thumbnailContainerStyle: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 10,
-        marginRight: 10
-
-    },
-    imageStyle: {
-        height: 300,
-        flex: 1,
-        width: null
-
-    }
-}
+export default SignUp
