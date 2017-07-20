b19c2439dd89b53acafc5c4449521005810e4bfd
BIWhitfield <beniwhitfield@gmail.com>
2017-07-20 16:02:17 +0100

    [GroupCard] revert component display

--- src/components/GroupCard.js
+++ src/components/GroupCard.js
@@ -1,47 +1,18 @@
 import React from 'react';
-import { Text, View, Image, Linking} from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 
 import Card from './common/Card';
 import CardSection from './common/CardSection';
 
 
 const GroupCard = ({ group }) => {
-    const { groupBanner, groupName , admin } = group;
-    const { 
-        headerContentStyle,
-        headerTextStyle,
-        imageStyle
-        } = styles;
+
     return (
-        <Card>
-            <CardSection>
-                    <Image style={imageStyle}
-                    source={{uri: groupBanner}} />
-                    </CardSection>
-                    <CardSection>
-                <View style={headerContentStyle}>
-                    <Text style={headerTextStyle}>{groupName}</Text>
-                    <Text>Admin: {admin}</Text>
-                </View>
-            </CardSection>
-        </Card>
+        <View>
+            <Text>A GROUP</Text>
+        </View>
     )
 };
 
-const styles = {
-    headerContentStyle: {
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 18
-    },
-    imageStyle: {
-        height: 150,
-        flex: 1,
-        width: null
-
-    }
-}
 
 export default GroupCard;
