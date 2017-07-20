65d600710f2912ec3ad941fc993720b79d32ea9e
BIWhitfield <beniwhitfield@gmail.com>
2017-07-20 16:02:31 +0100

    [EventCard] revert component display

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -5,39 +5,15 @@ import Card from './common/Card';
 import CardSection from './common/CardSection';
 
 
-const EventCard = ({ event }) => {
-    const { date, eventName , image } = event;
-    const { 
-        headerContentStyle,
-        headerTextStyle,
-        imageStyle
-        } = styles;
+const EventCard = () => {
+
     return (
-        <Card>
-            <CardSection>
-                <View style={headerContentStyle}>
-                    <Text style={headerTextStyle}>{eventName}</Text>
-                    <Text>{date}</Text>
-                </View>
-            </CardSection>
-        </Card>
+        <View>        
+            <Text>AN EVENT</Text>
+        </View>
     )
 };
 
-const styles = {
-    headerContentStyle: {
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 18
-    },
-    imageStyle: {
-        height: 300,
-        flex: 1,
-        width: null
 
-    }
-}
 
 export default EventCard;
