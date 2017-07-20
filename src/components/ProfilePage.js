909742ca6757da01ad3fd21655f4fe3a64942491
BIWhitfield <beniwhitfield@gmail.com>
2017-07-20 16:00:46 +0100

    [ProfilePage] revert component display

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,76 +1,38 @@
 import React from 'react';
-import { Text, View, Image, Linking } from 'react-native';
+import { StyleSheet, Text, View, Image, Linking } from 'react-native';
 
 import Card from './common/Card';
 import CardSection from './common/CardSection';
+import { Actions } from 'react-native-router-flux';
 
 
 const ProfilePage = () => {
 
-    const { thumbnailStyle,
-        headerContentStyle,
-        thumbnailContainerStyle,
-        headerTextStyle,
-        imageStyle
-        } = styles;
-    return (
-        <Card>
-            <CardSection>
-                <Image
-                    style={imageStyle}
-                    source={{ uri: 'https://i.ytimg.com/vi/RXKJolS9Atg/hqdefault.jpg' }} />
-            </CardSection>
-            <CardSection>
-                <View style={headerContentStyle}>
-                    <Text style={headerTextStyle}>User Profile</Text>
-                    <Text>Dick Dastardly</Text>
-                </View>
-            </CardSection>
-            <CardSection>
-                <View>
-                    <Text>Volunteer Level: 200</Text>
-                </View>
-            </CardSection>
-                <CardSection>
-                    <View>
-                        <Text>Create Group</Text>
-                    </View>
-                </CardSection>
-                <CardSection>
-                    <View>
-                        <Text>EVENTS GROUPS ALERTS SEARCH SETTINGS</Text>
-                    </View>
-                </CardSection>
-        </Card>
-    )
+	return (
+		<View style={styles.container}>
+			<Text 
+				style={styles.profile}>
+				PROFILE PAGE
+			</Text>
+		</View>
+	)
 };
 
 const styles = {
-    headerContentStyle: {
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 18
-    },
-    thumbnailStyle: {
-        height: 50,
-        width: 50
-
-    },
-    thumbnailContainerStyle: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 10,
-        marginRight: 10
-
-    },
-    imageStyle: {
-        height: 300,
-        flex: 1,
-        width: null
-
-    }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#bb3000'
+	},
+	profile: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		color: '#ffffff'	
+	}
 }
+
+
 
 export default ProfilePage;
