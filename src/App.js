06e5ba23a5963b35f2fea0f880ee709f52b46e43
BIWhitfield <beniwhitfield@gmail.com>
2017-07-20 16:04:03 +0100

    [App] Add routes for nav bar at bottom of screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
 import React, { Component } from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
+	StyleSheet,
+	Text,
+	PixelRatio,
+	View
 } from 'react-native';
-import CreateGroups from './components/CreateGroups';
-import ProfilePage from './components/ProfilePage';
-import AddEvent from './components/AddEvent';
-import Notifications from './components/Notifications';
-import GroupPage from './components/GroupPage';
-import MyGroups from './components/MyGroups';
 
-class App extends Component {
-    render() {
-        return (
-<MyGroups />
-          
-        );
-    };
+import { Scene, Router } from 'react-native-router-flux'
+
+import ProfilePage from './components/ProfilePage';
+import MyGroups from './components/MyGroups';
+import Calendar from './components/Calendar';
+import Notifications from './components/Notifications';
+import Search from './components/Search';
+
+
+const TabIcon = ({ selected, title }) => {
+	return (
+		<Text style={{ color: selected ? 'red' : 'black' }}>{title}</Text>
+	);
+};
+
+const App = () => {
+
+	return (
+		<Router>
+			<Scene key="root" style={styles.container}>
+				<Scene
+					key="tabbar"
+					tabs
+					tabBarStyle={styles.tabBar}
+				>
+
+					<Scene key="calendar" title="CALENDAR" icon={TabIcon}>
+						<Scene
+							key="calendar"
+							component={Calendar}
+							title="Calendar"
+							initial
+						/>
+					</Scene>
+
+					<Scene key="myGroups" title="GROUPS" icon={TabIcon}>
+						<Scene
+							key="myGroups"
+							component={MyGroups}
+							title="My Groups"
+						/>
+					</Scene>
+
+					<Scene key="home" title="HOME" icon={TabIcon}>
+						<Scene
+							key="profilePage"
+							component={ProfilePage}
+							title="Profile Page"
+							initial
+						/>
+					</Scene>
+
+					<Scene key="notifications" title="NOTIFICATIONS" icon={TabIcon}>
+						<Scene
+							key="notifications"
+							component={Notifications}
+							title="Notifications"
+						/>
+					</Scene>
+
+					<Scene key="search" title="SEARCH" icon={TabIcon}>
+						<Scene
+							key="search"
+							component={Search}
+							title="Search"
+						/>
+					</Scene>
+
+				</Scene>
+			</Scene>
+		</Router>
+	);
+}
+
+const styles = {
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FcFF'
+	},
+	appText: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		color: '#ffffff'
+	}
 }
 
 export default App;
