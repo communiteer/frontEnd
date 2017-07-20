75b3f30dc6f57ac244c78a2fd68e70e66e661ef2
BIWhitfield <beniwhitfield@gmail.com>
2017-07-20 16:01:16 +0100

    [Notifications] revert component display

--- src/components/NotificationDetail.js
+++ src/components/NotificationDetail.js
@@ -5,39 +5,16 @@ import Card from './common/Card';
 import CardSection from './common/CardSection';
 
 
-const NotificationDetail = ({ message }) => {
-    const { GroupName, skill, Message , image } = message;
-    const { 
-        headerContentStyle,
-        headerTextStyle,
-        imageStyle
-        } = styles;
+const NotificationDetail = () => {
+
     return (
-        <Card>
-            <CardSection>
-                <View style={headerContentStyle}>
-                    <Text style={headerTextStyle}>{GroupName}</Text>
-                    <Text>{Message}</Text>
-                </View>
-            </CardSection>
-        </Card>
+        <View>
+            <Text>
+                A NOTIFICATION IN DETAIL
+            </Text>
+        </View>
     )
 };
 
-const styles = {
-    headerContentStyle: {
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-    },
-    headerTextStyle: {
-        fontSize: 18
-    },
-    imageStyle: {
-        height: 300,
-        flex: 1,
-        width: null
-
-    }
-}
 
 export default NotificationDetail;
