bfde3a9edcfb1a3412ec070ce0bf92ac5d0e69e2
BIWhitfield <beniwhitfield@gmail.com>
2017-07-22 15:34:14 +0100

    [App] refactore to extract routes to new component

--- src/App.js
+++ src/App.js
@@ -5,113 +5,30 @@ import {
 	PixelRatio,
 	View
 } from 'react-native';
-
-import Icon from 'react-native-vector-icons/FontAwesome';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import Thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import reducers from './reducers';
 import { Scene, Router } from 'react-native-router-flux'
-
-import ProfilePage from './components/ProfilePage';
-import MyGroups from './components/MyGroups';
-import CalendarPage from './components/CalendarPage';
-import Notifications from './components/Notifications';
-import Search from './components/Search';
-import AddEvent from './components/AddEvent';
-import AddGroup from './components/AddGroup';
-import EventCard from './components/EventCard';
+import RouterComponent from './Router';
 
 
-function CalendarIcon(props) {
-	return (
-		<View>
-			<Icon name="calendar" size={30} color="#900" />
-		</View>
-	)
-}
 
-
-function GroupsIcon(props) {
-	return (
-		<View>
-			<Icon name="group" size={30} color="#900" />
-		</View>
-	)
-}
-
-function HomeIcon(props) {
-	return (
-		<View>
-			<Icon name="home" size={30} color="#900" />
-		</View>
-	)
-}
-
-function NotificationsIcon(props) {
-	return (
-		<View>
-			<Icon name="flag" size={30} color="#900" />
-		</View>
-	)
-}
-
-function SearchIcon(props) {
-	return (
-		<View>
-			<Icon name="search" size={30} color="#900" />
-		</View>
-	)
-}
 
 
 class App extends Component {
-render () {
-	return (
-		<Router>
-			<Scene key="root">
-				<Scene
-					key="tabbar"
-					tabs
-				>
-
-					<Scene tabs key="calendar" title="CALENDAR" icon={CalendarIcon} component={CalendarPage} hideNavBar>
-					</Scene>
-
-					<Scene tabs key="myGroups" title="GROUPS" icon={GroupsIcon} component={MyGroups} hideNavBar>
-					</Scene>
-
-					<Scene tabs initial key="home" title="HOME" icon={HomeIcon} component={ProfilePage} hideNavBar>
-					</Scene>
-
-					<Scene tabs key="notifications" title="NOTIFICATIONS" icon={NotificationsIcon} component={Notifications} hideNavBar>
-					</Scene>
-
-					<Scene tabs key="search" title="SEARCH" icon={SearchIcon} component={Search} hideNavBar>
-					</Scene>
-
-				</Scene>
-						<Scene key="addGroup" component={AddGroup} title="Add Group!"/>
-						<Scene key="addEvent" component={AddEvent} title="Add Event!"/>
-						<Scene key="anEvent" component={EventCard} title="The Event"/>
-			</Scene>
-		</Router>
-	);
-
-}
-}
-
-const styles = {
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FcFF'
-	},
-	appText: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-		color: '#ffffff'
+	render () {
+		const store = createStore(reducers, {}, applyMiddleware(Thunk))
+		return (
+			<Provider store={store}>
+				<RouterComponent />
+			</Provider>
+		);
 	}
 }
+
+
 
 export default App;
 
