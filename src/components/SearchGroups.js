d48763e375edcd91357b9b1e80dba307cc7baa87
BIWhitfield <beniwhitfield@gmail.com>
2017-07-24 12:15:24 +0100

    [SearchGroups] initial commit

---
+++ src/components/SearchGroups.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Text, View, ScrollView } from 'react-native';
+import { FetchAllGroupsInArea, FetchAllEventsInArea } from '../actions'
+
+import { Header, Button, CardSection } from './common';
+import GroupCard from './GroupCard';
+
+import { Actions } from 'react-native-router-flux';
+import * as actions from '../actions';
+
+const areaId = 1;
+
+class SearchGroups extends Component {
+constructor(props) {
+	super(props)
+}
+
+	componentDidMount() {
+
+		this.props.fetchAllGroupsInArea()
+
+	}
+
+	render() {
+		
+		return (
+			<View>
+			<ScrollView>
+						{this.props.groups.groups.map(group => {
+							return (
+								<View key={group.group_id}>
+									<GroupCard group={group} />
+								</View>
+							)
+						})}
+			</ScrollView>
+			</View>
+		)
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		fetchAllGroupsInArea: () => {
+			dispatch(actions.fetchAllGroupsInArea(areaId))
+		}
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		groups: state.groups
+	}
+}
+
+const styles = {
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#bb3000'
+	},
+	search: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+		color: '#ffffff'
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchGroups);
