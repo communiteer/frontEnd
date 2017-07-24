510cdd99a37945f68b704c61c78ab66d88d0b91f
BIWhitfield <beniwhitfield@gmail.com>
2017-07-24 15:10:30 +0100

    [index(reducers)] add [DisplayGroupPage] reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import UserProfileReducer from './UserProfileReducer';
 import GroupsInAreaReducer from './GroupsInAreaReducer';
 import EventsInAreaReducer from './EventsInAreaReducer';
+import DisplayGroupPageReducer from './DisplayGroupPageReducer';
 
 export default combineReducers({
   user: UserProfileReducer,
   groups: GroupsInAreaReducer,
-  events: EventsInAreaReducer
+  events: EventsInAreaReducer,
+  group: DisplayGroupPageReducer
 })
