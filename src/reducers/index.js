7da51b4bf9964df2c62126e23e175ddadfa1423f
BIWhitfield <beniwhitfield@gmail.com>
2017-07-25 10:35:33 +0100

    [index(reducer] add [DisplayEventSkillsReducer]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import UserProfileReducer from './UserProfileReducer';
 import GroupsInAreaReducer from './GroupsInAreaReducer';
 import EventsInAreaReducer from './EventsInAreaReducer';
 import DisplayGroupPageReducer from './DisplayGroupPageReducer';
+import DisplayEventSkillsReducer from './DisplayEventSkillsReducer';
 
 export default combineReducers({
   user: UserProfileReducer,
   groups: GroupsInAreaReducer,
   events: EventsInAreaReducer,
-  group: DisplayGroupPageReducer
+  group: DisplayGroupPageReducer,
+  eventSkills: DisplayEventSkillsReducer
 })
