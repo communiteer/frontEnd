a8ecad81bee555e933257005c976bcffd46ceeed
BIWhitfield <beniwhitfield@gmail.com>
2017-07-25 17:55:48 +0100

    [index(reducers)] add createGroupForm reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@ import GroupsInAreaReducer from './GroupsInAreaReducer';
 import EventsInAreaReducer from './EventsInAreaReducer';
 import DisplayGroupPageReducer from './DisplayGroupPageReducer';
 import DisplayEventSkillsReducer from './DisplayEventSkillsReducer';
+import AddGroupReducer from './AddGroupReducer';
 
 export default combineReducers({
   user: UserProfileReducer,
   groups: GroupsInAreaReducer,
   events: EventsInAreaReducer,
   group: DisplayGroupPageReducer,
-  eventSkills: DisplayEventSkillsReducer
+  eventSkills: DisplayEventSkillsReducer,
+  createGroupForm: AddGroupReducer
 })
