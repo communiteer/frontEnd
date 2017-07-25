26682516f812669e8db34024d158804b6af8fe31
BIWhitfield <beniwhitfield@gmail.com>
2017-07-25 17:58:35 +0100

    [AddGroup] implement state, props and form to create new group

--- src/components/AddGroup.js
+++ src/components/AddGroup.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Linking } from 'react-native';
-
-import Card from './common/Card';
-import CardSection from './common/CardSection';
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
+import { connect } from 'react-redux'
+import { addGroupUpdate, addGroupCreate } from '../actions/AddGroupAction'
+import { Card, CardSection, Input, Button } from './common';
 import { Actions } from 'react-native-router-flux';
 
-const AddGroup = () => {
-    return (
-        <View>
-            <Text>Add Group Page</Text>
-        </View>
-    )
+class AddGroup extends Component {
+	onButtonPress() {
+		// console.log(this.props)
+		const { group_name, group_Id, description, contact_details, user } = this.props;
+		
+		this.props.addGroupCreate({ 
+			group_name, 
+			group_Id, 
+			description, 
+			contact_details, 
+			user_id: user.user_id, 
+			area: user.area 
+		})
+	}
+
+	render() {
+		const {updateGroup} = this.props;
+		return (
+			<Card>
+				<CardSection>
+					<Input
+						label="Group Name"
+						placeholder="Group name"
+						value={this.props.group_name}
+						onChangeText={value => updateGroup({prop: 'group_name', value})}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input 
+						label="Description"
+						placeholder="Group Description"
+						value={this.props.description}
+						onChangeText={value => updateGroup({prop: 'description', value})}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input 
+						label="Phone Number"
+						placeholder="+44 6666 666666"
+						value={this.props.contact_details}
+						onChangeText={value => updateGroup({prop: 'contact_details', value})}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button onPress={this.onButtonPress.bind(this)}>
+						Create Group
+					</Button>
+				</CardSection>
+			</Card>
+		)
+	}
 }
 
+const mapStateToProps = (state) => {
+	const { group_name, group_Id, description, contact_details } = state.createGroupForm;
+	return { group_name, group_Id, description, contact_details, user: state.user }
+}
 
-
-export default AddGroup;
+export default connect(mapStateToProps, {updateGroup: addGroupUpdate, addGroupCreate})(AddGroup);
