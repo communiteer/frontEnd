9db45b50e1c87a68d369eda985a2cad4e7893ff6
Leo Duffy <leoduffy@hotmail.com>
2017-07-25 15:02:31 +0100

    [reducers] added userGroups

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
 import UserProfileReducer from './UserProfileReducer';
+import UserEventsReducer from './UserEventsReducer';
+import UserGroupsReducer from './UserGroupsReducer';
+import UserNotificationsReducer from './UserNotificationsReducer';
 
 export default combineReducers({
-  user: UserProfileReducer
+  user: UserProfileReducer,
+  events: UserEventsReducer,
+  groups: UserGroupsReducer,
+  notifications: UserNotificationsReducer
 })
